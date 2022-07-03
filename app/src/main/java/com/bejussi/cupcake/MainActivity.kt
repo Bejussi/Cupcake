4f6fd03cc54b3d8bfeb68ab39d052de293008331
Alexandra <pindooqy@gmail.com>
2022-07-03 13:42:13 +0300

    Create Project

---
+++ app/src/main/java/com/bejussi/cupcake/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bejussi.cupcake
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
